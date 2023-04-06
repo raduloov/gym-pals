5ee9e0629c43ec63fafa3ff2d6a530ad7ed29178
Yavor Radulov <raduloov@gmail.com>
2023-04-06 10:44:49 +0300

    fix posting for google user

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -28,7 +28,7 @@ const addUserDataToPosts = async (posts: Post[]) => {
     if (!author || !author.username)
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
-        message: "Author for post not found",
+        message: `Author for post not found ${JSON.stringify(users[0])}`,
       });
 
     return {
