6c274eb458a975825f29f2507c5b81d02c4ab048
Yavor Radulov <raduloov@gmail.com>
2023-04-07 18:19:13 +0300

    fix build

--- src/pages/create-workout/WorkoutBuilder.tsx
+++ src/pages/create-workout/WorkoutBuilder.tsx
@@ -121,58 +121,59 @@ const WorkoutBuilder = ({
       )}
 
       <div className="m-4 flex flex-wrap justify-center gap-10">
-        {selectedExercises.map((exercise, exerciseIndex) => (
-          <div className="text-xl" key={exercise.name}>
-            {`${exerciseIndex + 1}. ${exercise.name}`}
-            <div className="my-1 flex flex-col">
-              {exercise.sets.map((set, setIndex) => (
-                <div className="my-1 flex" key={setIndex}>
-                  <input
-                    type="number"
-                    placeholder="0"
-                    value={set.reps}
-                    onChange={(e) =>
-                      handleRepsChange(
-                        e.target.valueAsNumber,
-                        set,
-                        exerciseIndex,
-                        setIndex
-                      )
-                    }
-                    min={0}
-                    className="mr-2 h-8 w-12 p-1 text-black"
-                  />
-                  reps @
-                  <input
-                    type="number"
-                    placeholder="0"
-                    value={set.weight}
-                    onChange={(e) =>
-                      handleWeightChange(
-                        e.target.valueAsNumber,
-                        set,
-                        exerciseIndex,
-                        setIndex
-                      )
-                    }
-                    min={0}
-                    className="mx-2 h-8 w-16 p-1 text-black"
-                  />
-                  kg
-                </div>
-              ))}
+        {selectedExercises &&
+          selectedExercises.map((exercise, exerciseIndex) => (
+            <div className="text-xl" key={exercise.name}>
+              {`${exerciseIndex + 1}. ${exercise.name}`}
+              <div className="my-1 flex flex-col">
+                {exercise.sets.map((set, setIndex) => (
+                  <div className="my-1 flex" key={setIndex}>
+                    <input
+                      type="number"
+                      placeholder="0"
+                      value={set.reps}
+                      onChange={(e) =>
+                        handleRepsChange(
+                          e.target.valueAsNumber,
+                          set,
+                          exerciseIndex,
+                          setIndex
+                        )
+                      }
+                      min={0}
+                      className="mr-2 h-8 w-12 p-1 text-black"
+                    />
+                    reps @
+                    <input
+                      type="number"
+                      placeholder="0"
+                      value={set.weight}
+                      onChange={(e) =>
+                        handleWeightChange(
+                          e.target.valueAsNumber,
+                          set,
+                          exerciseIndex,
+                          setIndex
+                        )
+                      }
+                      min={0}
+                      className="mx-2 h-8 w-16 p-1 text-black"
+                    />
+                    kg
+                  </div>
+                ))}
+              </div>
+              <div className="flex">
+                <button
+                  onClick={() => handleAddSet(exerciseIndex)}
+                  type="button"
+                  className="mb-2 mr-2 rounded-lg border border-blue-700 px-3 py-2 text-center text-xs font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                >
+                  + Add set
+                </button>
+              </div>
             </div>
-            <div className="flex">
-              <button
-                onClick={() => handleAddSet(exerciseIndex)}
-                type="button"
-                className="mb-2 mr-2 rounded-lg border border-blue-700 px-3 py-2 text-center text-xs font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
-              >
-                + Add set
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       {selectedWorkoutType && !isSelectingExercises && (
